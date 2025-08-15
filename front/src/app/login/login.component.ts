35d3e0916332e7cfd521d33a25b7652cbe667e48
Lyes <58575102+LyesOrd@users.noreply.github.com>
2025-08-15 23:33:23 +0200

    feat(front): improve login page styling
    * feat: style login page
    
    * fix(front): make login inputs same width
    
    * feat: new style for login page

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { InputGroupModule } from 'primeng/inputgroup';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NavbarComponent,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    DividerModule,
+    InputGroupModule,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
