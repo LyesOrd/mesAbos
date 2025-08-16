6e4a827a4a752534e243f4fabb0f82b95afba839
LyesOrd <lyes.ord@gmail.com>
2025-08-16 13:44:13 +0200

    feat: adding jwt token + env file

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -11,8 +11,10 @@ import { PrismaService } from '../prisma.service';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET') || 'change-me',
-        signOptions: { expiresIn: '10m' },
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: config.get<string>('JWT_EXPIRES_IN') || '10min',
+        },
       }),
     }),
   ],
