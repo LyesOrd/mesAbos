c06b34d85c9ad6d01e97ffc7f8e8dfe0f5daa630
Lyes <58575102+LyesOrd@users.noreply.github.com>
2025-08-22 15:38:41 +0200

    test: add RouterTestingModule to navbar spec

--- front/src/app/shared/navbar/navbar.component.spec.ts
+++ front/src/app/shared/navbar/navbar.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 import { AuthService } from '../../auth.service';
-import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -11,11 +11,8 @@ describe('NavbarComponent', () => {
   beforeEach(async () => {
     const authSpy = jasmine.createSpyObj('AuthService', ['isLoggedIn', 'logout']);
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent],
-      providers: [
-        { provide: AuthService, useValue: authSpy },
-        { provide: Router, useValue: jasmine.createSpyObj('Router', ['navigate']) },
-      ],
+      imports: [RouterTestingModule, NavbarComponent],
+      providers: [{ provide: AuthService, useValue: authSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarComponent);
