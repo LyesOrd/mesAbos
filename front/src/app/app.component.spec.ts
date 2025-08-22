3421f85b8c05eb23737556d1bf56e3a3957956a6
Lyes <58575102+LyesOrd@users.noreply.github.com>
2025-08-22 15:41:43 +0200

    fix: update app component spec (#35)

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('front');
   });
 
-  it('should render title', () => {
+  it('should render a router outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, front');
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
