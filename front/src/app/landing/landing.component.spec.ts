3e16a4ae22338dbb159c325ca67d80deee8a552c
Lyes <58575102+LyesOrd@users.noreply.github.com>
2025-08-22 15:38:25 +0200

    fix: use router testing utilities in landing spec

--- front/src/app/landing/landing.component.spec.ts
+++ front/src/app/landing/landing.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LandingComponent } from './landing.component';
 import { AuthService } from '../auth.service';
-import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -9,10 +9,9 @@ describe('LandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingComponent],
+      imports: [LandingComponent, RouterTestingModule],
       providers: [
         { provide: AuthService, useValue: { isLoggedIn: () => false, logout: () => {} } },
-        { provide: Router, useValue: { navigate: () => {} } },
       ],
     }).compileComponents();
 
